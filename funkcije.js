b7c4f083f8e47aad47057b9f1264d419f07e9a94
codename11 <veljkos82@gmail.com>
2018-03-24 04:13:48 +0100

    Added support for json.

--- funkcije.js
+++ funkcije.js
@@ -1,130 +1,107 @@
-function serialization(phpdoc,me,choice){
+function serialization(phpdoc,forma,choice,elemName){
 	/*First argument is where data is sent to, second is keyword 'this', third is boolean value. 
 	If it's false, it is sent as ordinary formated AJAX string. If it's true, it is sent as json string.
-	It works on these input types: checkbox, radio, text, number, textarea and email.*/
-	var FormId = me.form.id;
+	Forth one is an id of an element where ajax response from server you want to be displayed.
+	It works on these input types: checkbox, radio, text, number, textarea and email.
+	Word of caution: If there's a radio button/s on page, one of them need to have attribute checked.
+	*/
+	var formax = forma;
 	
 	var obj = {
 			"name" : [],
 			"value" : []
 		};
 
+	var Wboard = document.getElementById(elemName);
 	var name = "";
 	var val = "";
-	var str = phpdoc+"?";
+	var doc = phpdoc+"?"
+	var str = "";
 	var i = 0;
-	var temp1 = "";
-	var temp2 = "";
-	
-	var net = me.form.querySelectorAll("#"+FormId+" [name]");
+
+	var net = formax.querySelectorAll("#"+formax.id+" [name]");
 	var len = net.length;
-	
+
 	while(i<len) {
 
-		if(net[i].type=="radio" && net[i].checked===true && net[i].value!=""){
-			name = net[i].name;
-			val = net[i].value;	
-		}
-		
-		if(net[i].type=="radio" && net[i].checked===false){
-			net[i].name = "";
-			net[i].value = "";
+		if(net[i].type=="radio" && net[i].checked===true){//Checking if radio button is checked.
 			name = net[i].name;
 			val = net[i].value;	
 		}
 	
-		if(net[i].type=="checkbox" && net[i].checked===true){
+		if(net[i].type=="checkbox" && net[i].checked===true){//Checking if checkbox is checked. If it's true, assign name and value.
 			name = net[i].name;
 			val = net[i].value;	
 		}
 		
-		if(net[i].type=="checkbox" && net[i].checked===false){
+		if(net[i].type=="checkbox" && net[i].checked===false){//Checking if checkbox is checked. If it's true, assign name and value. In tis case, value is empty string.
 			name = net[i].name;
-			net[i].value = "";
-			val = net[i].value;	
+			val = "";	
 		}
 		
-		if(net[i].type!="checkbox" && net[i].type!="button" && net[i].type!="radio"){
+		if(net[i].type!="checkbox" && net[i].type!="button" && net[i].type!="radio"){//If none of form inputs are of these types assign them value.
 			name = net[i].name;
 			val = net[i].value;
 		}
 		
+		str += name+"="+val;//Adding collected names and values to string which is used as a entry point for sending data either as ordinary string or json string.
+
+		if(i<len-1){
+			str += "&";//Adding sign after each assigned value to the name even if values is empty string.
+		}
+
 		i++;
+	}
+
+	var res = str.split("&");//Splitting string to array to remove duplicates.
+	var resLen = res.length-1;//Length of said array.
+	res = res.filter(function(elem, index, self) {//Duplicate filtration.
+		return index === self.indexOf(elem);
+	});
+	
+	str = "";//Resseting string and adding elements wthout duplicates.
+	for(var i=0;i<resLen;i++){//Iterating through array, separating names from values by determined coordinates of equal sign. Names and values are then appended to string and to JS object. 
 		
-		if(i<len){
-			
-			str += "&";
+		if(res[i]!==undefined){
+			str += res[i];//Appending filtered names and values.
+			var Eqsign = res[i].indexOf("=");//Locating coordinates of equal sign.
+			obj.name[i] = res[i].substring(0,Eqsign);//Assigning names to name subarray after "substring it" without equal sign.
+			obj.value[i] = res[i].substring(Eqsign+1);//Assigning values to value subarray after "substring it" without equal sign.
 		}
 		
-		str += name+"="+val;
-
-	}
-	
-	var str_len=str.length;
-	var last_char = str.substring((str_len-1), str_len);//if last char in str is '='
-	
-	var unwanted1 = str.indexOf("&=");
-	var unwanted2 = str.indexOf("?&");
-	
-	if(last_char==="="){
+		if(i<resLen-1){
+			str += "&";//Adding sign after every appendage except to last one.
+		}
 		
-		temp1 = str.substring(0, str_len-1);
-		str = temp1;
 	}
 	
-	if(unwanted1>(-1)){
-		
-		var temp1 = str.substring(0,unwanted1+1);
-		var temp2 = str.substring(unwanted1+2);
-		str = temp1+temp2;
-	}
-	
-	if(unwanted2>(-1)){
-
-		var temp1 = str.substring(0,unwanted2+1);
-		var temp2 = str.substring(unwanted2+2);
-		str = temp1+temp2;
-	}
-
-	str = encodeURI(str);
+	var jason = JSON.stringify(obj);//Parsing previously said object to json string.
+	str = encodeURI(doc+str);//Creating string suitable for sending it with ordinary ajax request.
 	str1 = str;
 	
 	var xhttp = new XMLHttpRequest();
 			
 	xhttp.onreadystatechange = function() {
 		
-		if((document.getElementById("raport")!=undefined || document.getElementById("raport")!=null) && this.readyState == 4 && this.status == 200){
-			document.getElementById("raport").innerHTML = this.responseText;
+		if((Wboard!=undefined || Wboard!=null) && this.readyState == 4 && this.status == 200){
+			Wboard.innerHTML = this.responseText;
+			//document.getElementById(formax.id).reset(); 
+			
 		}
 		 
 	};
-		
+	
 	if(choice===false){
 		xhttp.open("GET", str, false);	
 		xhttp.send();
-		alert(str);
+		document.getElementById("str").innerHTML = str;
 	}
 		
 	if(choice===true){
-		var Qsign = str.indexOf("?");
-		var strx = "";
-		strx = str.substr(Qsign+1);
-		var res = strx.split("&");
-		
-		for(var i=0;i<res.length;i++){
-			
-			var Eqsign = res[i].indexOf("=");
-			
-			var x = res[i].substr(0,Eqsign);
-			var y = res[i].substr(Eqsign+1);
-			
-			obj.name[i] = x;
-			obj.value[i] = y;
+		xhttp.open("GET", doc+"jason="+jason,true);
+		xhttp.setRequestHeader("Content-Type", "application/json");
+		xhttp.send();
 
-		}
-		
-		var jason = JSON.stringify(obj);
-		alert(jason);
 	}
 			
 }
